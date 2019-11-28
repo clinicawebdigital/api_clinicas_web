ed90a0ccf7e0fb3a89487fe8552eb2e90f7a185b
douglas-cavalcante <henriquedouglas2803@gmail.com>
2019-11-28 00:49:11 -0300

    Adiciona seed de Company

--- database/seeds/CompanySeeder.js
+++ database/seeds/CompanySeeder.js
@@ -4,9 +4,53 @@ const Company = use('App/Models/Company')
 
 class CompanySeeder {
   async run () {
-    await Company.create({
-     name: 'Minha Empresa'
+    const company = await Company.create({
+     name: '',
     })
+
+   await company.openingHours().createMany([
+      {
+        day: '0',
+        description: 'Domingo',
+        start: '',
+        end: '',
+      },
+      {
+        day: '1',
+        description: 'Segunda-feira',
+        start: '',
+        end: '',
+      },
+      {
+        day: '2',
+        description: 'Terça-feira',
+        start: '',
+        end: '',
+      },
+      {
+        day: '3',
+        description: 'Quarta-feira',
+        start: '',
+        end: '',
+      },
+      {
+        day: '4',
+        description: 'Quinta-feira',
+        start: '',
+        end: '',
+      }, {
+        day: '5',
+        description: 'Sexta-feira',
+        start: '',
+        end: '',
+      },
+      {
+        day: '6',
+        description: 'Sábado-feira',
+        start: '',
+        end: '',
+      }
+    ])
   }
 }
 
