f79f69f31a575a791ac876fcb026f33cfe27bf07
Douglas Cavalcante <henriquedouglas2803@gmail.com>
2019-12-28 10:20:24 -0300

    Adiciona controller e rotas de Procedures

--- app/Controllers/Http/ProcedureController.js
+++ app/Controllers/Http/ProcedureController.js
@@ -1,42 +1,95 @@
-'use strict'
+"use strict";
 
-
-const Procedure = use('App/Models/Procedure')
-
+const Procedure = use("App/Models/Procedure");
 
 class ProcedureController {
   async index({ request }) {
-    const { term = '' } = request.get()
-    const procedures = await Procedure.query().where("name", "LIKE", "%" + term + "%").fetch()
-    return procedures
+    const { id } = request.get();
+
+    const procedures = await Procedure.query()
+      .where("partnership_id", id)
+      .orderBy("id", "desc")
+      .fetch();
+
+    return procedures;
   }
 
   async store({ request, response }) {
     const data = request.only([
-      'name',
-      'partnership_id',
-      'description',
-      'value',
-      'value_transferred',
-      'observation'
-    ])
+      "partnership_id",
+      "code",
+      "name",
+      "description",
+      "observation",
+      "value",
+      "value_transferred"
+    ]);
 
-    console.log(data)
     const procedureExists = await Procedure.query()
-      .where('name', data.name)
-      .andWhere('partnership_id', data.partnership_id)
-      .first()
+      .where("name", data.name)
+      .andWhere("partnership_id", data.partnership_id)
+      .first();
 
     if (!procedureExists) {
-      const procedure = await Procedure.create(data)
-      return procedure
+      const procedure = await Procedure.create(data);
+
+      return procedure;
     } else {
-      return response
-        .status(400)
-        .send({ err: { message: 'Essa procedimento já está cadastrada para esse convênio.' } })
+      return response.status(400).send({
+        err: {
+          message: "Essa procedimento já está cadastrada para esse convênio."
+        }
+      });
     }
   }
 
+  async show({ response, params }) {
+    try {
+      const procedure = await Procedure.findOrFail(params.id);
+      return procedure;
+    } catch (err) {
+      return response
+        .status(err.status)
+        .send({ err: { message: "Esse procedimento não existe." } });
+    }
+  }
+
+  async status({ response, params }) {
+    try {
+      const procedure = await Procedure.findOrFail(params.id);
+      procedure.status = !procedure.status;
+      await procedure.save();
+      return procedure;
+    } catch (err) {
+      return response
+        .status(err.status)
+        .send({ err: { message: "Esse procedimento não existe." } });
+    }
+  }
+
+  async update({ params, request, response }) {
+    try {
+      const procedure = await Procedure.findOrFail(params.id);
+      const data = request.only([
+        "code",
+        "name",
+        "description",
+        "observation",
+        "value",
+        "value_transferred"
+      ]);
+
+      procedure.merge(data);
+      await procedure.save();
+      return procedure;
+    } catch (err) {
+      return response.status(err.status).send({
+        err: {
+          message: "Esse procedimento nao está presente no banco de dados."
+        }
+      });
+    }
+  }
 }
 
-module.exports = ProcedureController
+module.exports = ProcedureController;
--- database/migrations/1575122779245_procedure_schema.js
+++ database/migrations/1575122779245_procedure_schema.js
@@ -1,33 +1,34 @@
-'use strict'
+"use strict";
 
 /** @type {import('@adonisjs/lucid/src/Schema')} */
-const Schema = use('Schema')
+const Schema = use("Schema");
 
 class ProcedureSchema extends Schema {
   up() {
-    this.create('procedures', (table) => {
-      table.increments()
+    this.create("procedures", table => {
+      table.increments();
       table
-        .integer('partnership_id')
+        .integer("partnership_id")
         .unsigned()
-        .references('id')
-        .inTable('partnerships')
-        .onUpdate('CASCADE')
-        .onDelete('CASCADE')
-        .notNullable()
-      table.string('name').notNullable()
-      table.string('description')
-      table.float('value')
-      table.float('value_transferred')
-      table.text('observation')
-      table.boolean('status').defaultTo(true)
-      table.timestamps()
-    })
+        .references("id")
+        .inTable("partnerships")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE")
+        .notNullable();
+      table.string("code");
+      table.string("name").notNullable();
+      table.string("description");
+      table.float("value");
+      table.float("value_transferred");
+      table.text("observation");
+      table.boolean("status").defaultTo(true);
+      table.timestamps();
+    });
   }
 
   down() {
-    this.drop('procedures')
+    this.drop("procedures");
   }
 }
 
-module.exports = ProcedureSchema
+module.exports = ProcedureSchema;
