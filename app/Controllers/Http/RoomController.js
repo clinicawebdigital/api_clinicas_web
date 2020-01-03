30a9e99c8c5b6699d05190e77919b447cbe48b34
Douglas Cavalcante <henriquedouglas2803@gmail.com>
2020-01-03 00:05:01 -0300

    Adiciona controller de Rooms

--- app/Controllers/Http/RoomController.js
+++ app/Controllers/Http/RoomController.js
@@ -1,73 +1,58 @@
-'use strict'
+"use strict";
 
-const Room = use('App/Models/Room')
+const Room = use("App/Models/Room");
 
 class RoomController {
-
-  async index ({ request }) {
-
-    const rooms = Room.query().where('active', '=', true).fetch()
-    return rooms
+  async index() {
+    const rooms = Room.all();
+    return rooms;
   }
 
-  async store ({ request, response }) {
-    const data = request.only(['name', 'observations'])
-    const room = await Room.create(data)
-    return room
+  async store({ request }) {
+    const data = request.only(["name", "observations"]);
+
+    const room = await Room.create(data);
+    return room;
   }
 
-  async show ({ params, response }) {
-    try{
-      const room = await Room.findOrFail(params.id)
-      return room
-    }catch(err){
-      return response
-        .status(err.status)
-        .send({ err: { message: 'Sala não existente.'}})
-    }
-  }
-
-  async update ({ params, request, response }) {
-    try{
-      const room = await Room.findOrFail(params.id)
-      const data = request.only('name','observations','active')
-
-      if(data.name){
-        const roomExists = await Room.findBy('name', data.name)
-
-        if(!roomExists){
-          room.merge(data)
-          await room.save()
-          return room
-        } else {
-          return response
-            .status(400)
-            .send({ err: { message: 'Esse nome de sala ja está cadastrado.' } })
-        }
-      } else {
-        return response
-          .status(400)
-          .send({ err: { message: 'O nome da sala é obrigatório.' } })
-      }
-
-      }
-    catch(err){
-      return response
-        .status(err.status)
-        .send({ err: { message: 'Essa sala não existe'}})
-    }
-  }
-
-  async destroy ({ params, response }) {
+  async show({ params }) {
     try {
-      const room = await Room.findOrFail(params.id)
-      await room.delete()
-    }catch(err) {
+      const room = await Room.findOrFail(params.id);
+      return room;
+    } catch (err) {
       return response
-      .status(err.status)
-      .send({ err: { message: 'Sala não existente.'}})
+        .status(err.status)
+        .send({ err: { message: "Sala não existente." } });
+    }
+  }
+
+  async update({ params, request, response }) {
+    try {
+      const room = await Room.findOrFail(params.id);
+      const data = request.only(["name", "observations"]);
+
+      room.merge(data);
+      await room.save();
+      return room;
+    } catch (err) {
+      return response
+        .status(err.status)
+        .send({ err: { message: "Essa sala não existe" } });
+    }
+  }
+
+  async status({ response, params }) {
+    try {
+      const room = await Room.findOrFail(params.id);
+      room.active = !room.active;
+      await room.save();
+      return room;
+    } catch (err) {
+      return response
+        .status(err.status)
+        .send({ err: { message: "Essa sala não existe." } });
     }
   }
 }
 
-module.exports = RoomController
+module.exports = RoomController;
